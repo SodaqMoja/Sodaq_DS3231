65a56369cb0e3e73f488ab2c277a347361f6e5bd
Kees Bakker <kees@sodaq.net>
2014-01-21 20:41:45 +0100

    Move chip specific defines to .cpp and make read/write register private

--- Sodaq_DS3231.cpp
+++ Sodaq_DS3231.cpp
@@ -16,6 +16,36 @@
 #define EPOCH_TIME_OFF 946684800  // This is 2000-jan-01 00:00:00 in epoch time
 #define SECONDS_PER_DAY 86400L
 
+#define DS3231_ADDRESS	      0x68 //I2C Slave address
+
+/* DS3231 Registers. Refer Sec 8.2 of application manual */
+#define DS3231_SEC_REG        0x00  
+#define DS3231_MIN_REG        0x01  
+#define DS3231_HOUR_REG       0x02
+#define DS3231_WDAY_REG       0x03
+#define DS3231_MDAY_REG       0x04
+#define DS3231_MONTH_REG      0x05
+#define DS3231_YEAR_REG       0x06
+
+#define DS3231_AL1SEC_REG     0x07
+#define DS3231_AL1MIN_REG     0x08
+#define DS3231_AL1HOUR_REG    0x09
+#define DS3231_AL1WDAY_REG    0x0A
+
+#define DS3231_AL2MIN_REG     0x0B
+#define DS3231_AL2HOUR_REG    0x0C
+#define DS3231_AL2WDAY_REG    0x0D
+
+#define DS3231_CONTROL_REG          0x0E
+#define DS3231_STATUS_REG           0x0F
+#define DS3231_AGING_OFFSET_REG     0x0F
+#define DS3231_TMP_UP_REG           0x11
+#define DS3231_TMP_LOW_REG          0x12
+
+#define EverySecond     0x01
+#define EveryMinute     0x02
+#define EveryHour       0x03
+
 ////////////////////////////////////////////////////////////////////////////////
 // utility code, some of this could be exposed in the DateTime API if needed
 
--- Sodaq_DS3231.h
+++ Sodaq_DS3231.h
@@ -13,36 +13,6 @@
 
 #include <stdint.h>
 
-#define DS3231_ADDRESS	      0x68 //I2C Slave address
-
-/* DS3231 Registers. Refer Sec 8.2 of application manual */
-#define DS3231_SEC_REG        0x00  
-#define DS3231_MIN_REG        0x01  
-#define DS3231_HOUR_REG       0x02
-#define DS3231_WDAY_REG       0x03
-#define DS3231_MDAY_REG       0x04
-#define DS3231_MONTH_REG      0x05
-#define DS3231_YEAR_REG       0x06
-
-#define DS3231_AL1SEC_REG     0x07
-#define DS3231_AL1MIN_REG     0x08
-#define DS3231_AL1HOUR_REG    0x09
-#define DS3231_AL1WDAY_REG    0x0A
-
-#define DS3231_AL2MIN_REG     0x0B
-#define DS3231_AL2HOUR_REG    0x0C
-#define DS3231_AL2WDAY_REG    0x0D
-
-#define DS3231_CONTROL_REG          0x0E
-#define DS3231_STATUS_REG           0x0F
-#define DS3231_AGING_OFFSET_REG     0x0F
-#define DS3231_TMP_UP_REG           0x11
-#define DS3231_TMP_LOW_REG          0x12
-
-#define EverySecond     0x01
-#define EveryMinute     0x02
-#define EveryHour       0x03
-
 
 
 
@@ -78,8 +48,6 @@ protected:
 class Sodaq_DS3231 {
 public:
     uint8_t begin(void);
-    uint8_t readRegister(uint8_t regaddress);
-    void writeRegister(uint8_t regaddress, uint8_t value);
 
     void setDateTime(const DateTime& dt);  //Changes the date-time
     void setEpoch(uint32_t ts); // Set the RTC using timestamp (seconds since epoch)
@@ -96,6 +64,9 @@ public:
 
     void convertTemperature();
     float getTemperature();
+private:
+    uint8_t readRegister(uint8_t regaddress);
+    void writeRegister(uint8_t regaddress, uint8_t value);
 };
 
 extern Sodaq_DS3231 rtc;
