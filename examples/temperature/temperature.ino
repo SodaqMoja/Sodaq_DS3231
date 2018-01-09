c1250057abfab35894cd2731694181ffc090b2d9
SRGDamia1 <sdamiano@stroudcenter.org>
2018-01-09 14:37:50 -0500

    updated temp example to display date/time

--- examples/temperature/temperature.ino
+++ examples/temperature/temperature.ino
@@ -3,17 +3,31 @@
 #include <Wire.h>
 #include "Sodaq_DS3231.h"
 
-void setup () 
+void setup ()
 {
     Serial.begin(57600);
     Wire.begin();
     rtc.begin();
 }
 
-void loop () 
-{
+void loop ()
+{    DateTime now = rtc.now(); //get the current date-time
+
+	Serial.print(now.year(), DEC);
+	Serial.print('/');
+	Serial.print(now.month(), DEC);
+	Serial.print('/');
+	Serial.print(now.date(), DEC);
+	Serial.print(' ');
+	Serial.print(now.hour(), DEC);
+	Serial.print(':');
+	Serial.print(now.minute(), DEC);
+	Serial.print(':');
+	Serial.print(now.second(), DEC);
+	Serial.println();
+
     rtc.convertTemperature();             //convert current temperature into registers
     Serial.print(rtc.getTemperature()); //read registers and display the temperature
-    Serial.println("deg C");
+    Serial.println(" deg C");
     delay(1000);
 }
